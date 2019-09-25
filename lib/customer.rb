ffeeecadc0e874c6bbf5af92ae6ac3419a331484
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-25 00:46:19 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -20,10 +20,10 @@ class Customer
   end
   
   def meals
-
+  array = []
     meals_count = Hash.new 
     Meal.all.each do |customer|
-      meals_count[customer].find(self) 
+      array << meals_count[customer].find(self) 
     end
   end
     
