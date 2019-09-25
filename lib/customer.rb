6e909b307c5414f292e2e07430a03769d9e12cef
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-25 00:40:25 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -23,7 +23,7 @@ class Customer
     meals_count = Hash.new 
     Meal.all.each do |customer|
         meals_count[customer] += 1
-        [customer]
+        meals_count[customer]
     end
   end
     
